b2938af92186e67b26ae64f325d3c5a34483c0c6
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-01-21 15:27:35 +0100

    chore: improve 11ty file copy

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,10 @@ module.exports = function (eleventyConfig) {
   // This allows Eleventy to watch for file changes during local development.
   eleventyConfig.setUseGitIgnore(false);
 
-  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/assets/fonts");
+  eleventyConfig.addPassthroughCopy("src/assets/images");
+  eleventyConfig.addPassthroughCopy("src/assets/css/style.compiled.css");
+  eleventyConfig.addPassthroughCopy({ "src/assets/common": "." });
 
   eleventyConfig.addWatchTarget("./src/assets/css/style.compiled.css");
 
