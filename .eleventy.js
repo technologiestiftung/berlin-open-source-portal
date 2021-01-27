e0b24b9631a14566af161e0cf38a06f91cd9efb6
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-01-27 09:25:56 +0100

    refactor(OSR-63): bring back old CSS (temporarily)

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,12 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/fonts");
   eleventyConfig.addPassthroughCopy("src/assets/images");
   eleventyConfig.addPassthroughCopy("src/assets/css/style.compiled.css");
+  // Legacy CSS files following. TODO: Remove when refactor to Tailwind is complete.
+  eleventyConfig.addPassthroughCopy("src/assets/css/fonts.css");
+  eleventyConfig.addPassthroughCopy("src/assets/css/github-markdown.css");
+  eleventyConfig.addPassthroughCopy("src/assets/css/syntax.css");
+  eleventyConfig.addPassthroughCopy("src/assets/css/style-old.css");
+  // Legacy CSS end.
   eleventyConfig.addPassthroughCopy({ "src/assets/common": "." });
 
   eleventyConfig.addWatchTarget("./src/assets/css/*.css");
